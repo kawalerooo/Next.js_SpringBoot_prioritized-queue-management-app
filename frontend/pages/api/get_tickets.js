238475a9230aa7c645671e015659ee81dd9e5033
kawalero <73931905+kawalerooo@users.noreply.github.com>
2023-06-14 16:54:33 +0200

    Delete frontend directory

--- frontend/pages/api/get_tickets.js
+++
@@ -1,9 +0,0 @@
-export default async function handler(req, res) {
-    try {
-        let res = await fetch(`http://localhost:8080/api/v1/tickets`);
-        return await res.json();
-    } catch (error) {
-        console.error(error)
-        return res.status(error.status || 500).end(error.message)
-    }
-}
